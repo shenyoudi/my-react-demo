ee1129345171cc9b9ad08574e0a98b6670a2f2c1
shenyoudi <shenyoudi@qq.com>
2021-06-11 16:58:23 +0800

    update

--- src/views/Page0/Page0.tsx
+++ src/views/Page0/Page0.tsx
@@ -1,7 +1,8 @@
-import {getList} from '@/utils/api'
-import {useState, useEffect} from 'react'
+import {Button, Input} from 'antd'
+import {useState, useEffect, useCallback} from 'react'
 import {connect} from 'react-redux'
 import {asyncGetUser} from '@/store/action/addUser'
+import './page0.scss'
 
 function mapStateToProps (state: any) {
 	return {
@@ -13,35 +14,56 @@ const mapActionsToProps = {
 	asyncGetUser
 }
 
-async function init () {
-	const list = await getList()
-	return list.data.data
-}
-
 function Page0 (props: any) {
 
-	const [list, setList] = useState([])
+	const [userInfo, setUserInfo] = useState({})
+
+	const getData = useCallback(async () => {
+		await props.asyncGetUser()
+	}, [])
+
+	const add = () => {
+		console.log(userInfo)
+	}
+
+	const del = () => {
+
+	}
+
+	const inputVal = (e: any, type: string) => {
+		const value = e.target.value
+		setUserInfo({...userInfo, [type]: type==='gender'? !!+value : value})
+	}
 
 	useEffect(() => {
-		props.asyncGetUser().then(() => {
-			console.log(props)
-		})
-	}, [])
+		getData()
+	}, [getData])
 
 	return (
 		<div>
+			<div className="add">
+				<Input className="add__input" onChange={(e) => inputVal(e, 'name')}></Input>
+				<Input className="add__input" onChange={(e) => inputVal(e, 'age')}></Input>
+				<Input className="add__input" onChange={(e) => inputVal(e, 'gender')}></Input>
+			</div>
+			<div>
+				<Button type="primary" onClick={add}>Add</Button>
+				<Button type="primary" onClick={del}>Del</Button>
+			</div>
 			<div>
 				<span>name</span>
 				<span>age</span>
 				<span>gender</span>
 				<span>address</span>
+				<span>date</span>
 			</div>
-			{list.length && list.map(({name, id, age, gender, address}: any) => (
+			{props.userList.length && props.userList.map(({name, id, age, gender, address, date}: any) => (
 				<div key={id}>
 					<span>{name}</span>
 					<span>{age}</span>
-					<span>{gender}</span>
+					<span>{gender? '♂' : '♀'}</span>
 					<span>{address}</span>
+					<span>{date}</span>
 				</div>
 			))}
 		</div>
--- src/views/Page1/Page1.tsx
+++ src/views/Page1/Page1.tsx
@@ -1,3 +1,29 @@
+import './page.scss'
+
+function HeightHalfWidth () {
+	return (
+		<div className='outer_wrapper'>
+			<div className='inner_wrapper'>
+				<div className='box'>height = width / 2</div>
+			</div>
+		</div>
+	)
+}
+
+function DivTextArea () {
+	return (
+		<div className='div-textarea' contentEditable='true'></div>
+	)
+}
+
 export default function Page1 () {
-	return (<div>Page1....</div>)
+	return (
+		<div className='parent'>
+			<div className='height-half-width-container'>
+				<HeightHalfWidth></HeightHalfWidth>
+			</div>
+			<h3>div textarea</h3>
+			<DivTextArea></DivTextArea>
+		</div>
+	)
 }
